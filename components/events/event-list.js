a5e349a180d1ddb5768975f20d0b6c9bf10ade85
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-08 10:59:05 +0200

    Style Components

--- components/events/event-list.js
+++ components/events/event-list.js
@@ -1,8 +1,10 @@
 import EventItem from './event-item';
 
+import classes from './event-list.module.css';
+
 function EventList({ items: events }) {
   return (
-    <ul>
+    <ul className={classes.list}>
       {events.map((event) => (
         <EventItem
           key={event.id}
