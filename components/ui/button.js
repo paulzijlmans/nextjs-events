109417ae667fe83a0d491426a4c24b33b524bece
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-09 11:30:24 +0200

    Filtered Events Page

--- components/ui/button.js
+++ components/ui/button.js
@@ -2,11 +2,15 @@ import Link from 'next/link';
 
 import classes from './button.module.css';
 
-function Button({ children, link }) {
-  return (
+function Button({ children, link, onClick }) {
+  return link ? (
     <Link href={link}>
       <a className={classes.btn}>{children}</a>
     </Link>
+  ) : (
+    <button className={classes.btn} onClick={onClick}>
+      {children}
+    </button>
   );
 }
 
--- pages/events/index.js
+++ pages/events/index.js
@@ -1,13 +1,23 @@
+import { useRouter } from 'next/router';
+
 import EventList from '../../components/events/event-list';
+import EventsSearch from '../../components/events/events-search';
 import { getAllEvents } from '../../data/dummy-data';
 
 function AllEventsPage() {
   const events = getAllEvents();
+  const router = useRouter();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+    router.push(fullPath);
+  }
 
   return (
-    <div>
+    <>
+      <EventsSearch onSearch={findEventsHandler} />
       <EventList items={events} />
-    </div>
+    </>
   );
 }
 
