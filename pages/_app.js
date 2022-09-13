8fb1ddc28d48b5213655b7162cda52b6120b38e7
Paul Zijlmans <paulzijlmans@hotmail.com>
2022-09-13 11:33:50 +0200

    Starting Setup App-wide State

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 
 import Layout from '../components/layout/layout';
+import Notification from '../components/ui/notification';
 
 import '../styles/globals.css';
 
@@ -13,6 +14,7 @@ function MyApp({ Component, pageProps }) {
         <meta name='viewport' content='inital-scale=1.0, width=device-width' />
       </Head>
       <Component {...pageProps} />
+      <Notification title='Test' message='This is a test.' status='pending' />
     </Layout>
   );
 }
